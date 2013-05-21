fabe90e6f298da85192c3094b77d455af4f267eb
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-05-21 18:48:12 +0200

    Revert "wireless: switch to bcmdhd (google)"
    This reverts commit e9c1d243ac879e5745e654d3ff6b74a0f9e748c7.

--- p4-common.mk
+++ p4-common.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory \
     power.p3 \
-    libnetcmdiface \
-    WiFiDirectDemo
+    libnetcmdiface
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
