087b04d8b2f796c885762e3da7a17be3c8739f5e
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-08-10 21:00:36 +0200

    Move vold-manager volumes to regular fstab

--- p4-common.mk
+++ p4-common.mk
@@ -33,7 +33,6 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/init.p3.usb.rc:root/init.p3.usb.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
