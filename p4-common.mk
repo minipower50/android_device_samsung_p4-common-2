5207866f78b6fad3a63a01690b6d994336f65f68
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-08-10 19:24:00 +0200

    Set ro.bq.gpu_to_cpu_unsupported to 1

--- p4-common.mk
+++ p4-common.mk
@@ -66,6 +66,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
     wifi.supplicant_scan_interval=15 \
     media.stagefright.cache-params=6144/-1/30 \
     ro.sf.lcd_density=160 \
+    ro.bq.gpu_to_cpu_unsupported=1 \
     dalvik.vm.dexopt-data-only=1 \
     debug.hwui.render_dirty_regions=false
 
