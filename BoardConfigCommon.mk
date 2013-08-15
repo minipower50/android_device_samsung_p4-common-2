2b257122ed621f991d77fbd13250afe6bc0ad705
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-08-15 08:59:46 +0200

    Revert "Update fstab to deal with removed recovery.fstab"
    This reverts commit 07b1ebe9f997ff229527fa7cf401969e20425b5e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,10 +74,6 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 # override recovery init.rc
 TARGET_RECOVERY_INITRC := device/samsung/p4-common/recovery/init.rc
 
-# Recovery.fstab
-TARGET_RECOVERY_FSTAB := device/samsung/p4-common/fstab.p3
-RECOVERY_FSTAB_VERSION := 2
-
 # Indicate that the board has an Internal SD Card
 BOARD_HAS_SDCARD_INTERNAL := true
 
