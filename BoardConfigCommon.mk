739ebcade3a47bf0840a80a51ce0d2c06de755e2
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-08-23 21:27:45 +0200

    Updated recovery fstab to new JB4.3 layout

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,9 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 # override recovery init.rc
 TARGET_RECOVERY_INITRC := device/samsung/p4-common/recovery/init.rc
 
+RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_FSTAB := device/samsung/p4-common/fstab.p3
+
 # Indicate that the board has an Internal SD Card
 BOARD_HAS_SDCARD_INTERNAL := true
 
